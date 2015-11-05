174bb4e91ce360c8e9ed83dbabe418d1ce7165cc
drdakjak <jak.drd@gmail.com>
2015-11-05 21:13:30 +0000

    tester 5.11.2015

--- testManager/testManager.py
+++ testManager/testManager.py
@@ -1,48 +1,65 @@
-import django.models
+from cryptocurrency_app.models import Pubticker
 import testExpertAdvisor as tEA
-def testManager(from_, to_):
+class TestManager:
       
-      trader_ = Trader_()
-      tEA.init(trader_)
+      wallet = {'USD':400,'BTC':1}
       
-      # loop
-      tEA.event(trader_)
+      def run_tester(self,from_, to_):
+            
+            pubticker_history = Pubticker.objects.all().filter(import_date_lte = to_).filter(import_date_gte=from_)
+            trader_ = Trader_(self.wallet)
+            tEA.init(trader_)
+            
+            for ticker in pubticker_history:
+                  trader_.pubticker_ = ticker
+                  tEA.event(trader_)
+            
+            
+            tEA.deinit(trader_)
+            
+            return self.wallet
       
-      
-      tEA.deinit(trader_)
-      
-      
-class Pubticker_:
-   def __init__(self, mid=None, bid=None, ask=None,last_price=None,low=None,high=None,volume=None,timestamp=None):
-        self.mid = mid
-        self.bid = bid
-        self.ask = ask
-        self.last_price = last_price
-        self.low = low
-        self.high = high
-        self.volume = volume
-        self.timestamp = timestamp   
 
 '''
 Basic interface class for testExpertAdvisor
 '''
 class Trader_:
       pubticker_ = None
-      broker_ = None
       order_id_ = None
       orders_ = {}
+      wallet = None
+      
+      def __init__(self,wallet):
+            self.wallet = wallet
             
-      def buy_(self):
-            self.orders_[self.order_id_] = Order_()
+      def buy_(self,amount):
+            price = self.pubticker_.bid
+            time = self.pubticker_.timestamp
+            self.orders_[self.order_id_] = Order_(price,time,"buy")
+            self.wallet['USD'] -= amount*price
+            self.wallet['BTC'] += amount
             self.order_id_ += 1
-            pass
-      def sell_(self):
-            self.orders_[self.order_id_] = Order_()
+            
+      def sell_(self,amount):
+            price = self.pubticker_.ask
+            time = self.pubticker_.timestamp
+            self.orders_[self.order_id_] = Order_(price,time,"sell")
+            self.wallet['USD'] += amount*price
+            self.wallet['BTC'] -= amount
             self.order_id_ += 1      
+            
+      def close_(self,order_id_):
             pass
-      def close_(self):
-            pass
-
+            # o = self.orders_[order_id_]
+            # o.active_ = False
+            # if(o.order_type_ == "buy"):
+            #       self.orders_[order_id_].close_price_ = pubticker_.ask
+            # if(o.order_type_ == "sell")
+            #       self.orders_[order_id_].close_price_ = pubticker_.bid
+            # self.orders_[order_id_].diff_price_
+            # self.orders_[order_id_].close_time_
+            # self.orders_[order_id_].diff_time_
+            
 '''
 Order class
 '''
@@ -62,9 +79,20 @@ class Order_:
       
       active_ = True
       
-      def __init__(self,open_price_,open_time_):
+      def __init__(self,open_price_,open_time_,order_type_):
             self.open_price_ = open_price_
             self.open_time_ = open_time_
+            self.order_type_ = order_type_
 
 
     
+# class Pubticker_:
+#    def __init__(self, mid=None, bid=None, ask=None,last_price=None,low=None,high=None,volume=None,timestamp=None):
+#         self.mid = mid
+#         self.bid = bid
+#         self.ask = ask
+#         self.last_price = last_price
+#         self.low = low
+#         self.high = high
+#         self.volume = volume
+#         self.timestamp = timestamp   
